dad01e22b4afd0d0ac61d74f02932b6a1ccee421
tonis ormisson <tonis@andmemasin.eu>
2022-12-01 17:43:14 +0200

    init GH actions

--- src/views/form.php
+++ src/views/form.php
@@ -2,7 +2,7 @@
 /** @var \yii\web\View $this */
 /** @var \tonisormisson\addressform\AddressForm $widget */
 
-use yii\bootstrap\ActiveForm;
+use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 
 $model = $widget->address;
